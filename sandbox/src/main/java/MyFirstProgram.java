2c4e6295c8df0288395006719d7a80b5120279eb
Zarina Karymbaeva <zkarymbaeva@gmail.com>
2020-04-09 21:06:08 +0200

    Dobavleno neskolko funkziy

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,26 @@
 public class MyFirstProgram{
 
-public static void main(String[] args){
-	System.out.println("Hello, world!");
+public static void main(String[] args)
+	{
+		hello("world");
+		hello("user");
+		hello("Alexei");
+		double i = 5;
+		double a = 5;
+		double b = 6;
+		System.out.println(" Ploshad kvadrata so storonoi: "+i+ "=" + area(i));
+		System.out.println(" Ploshad kvadrata so storonoi: "+a+ "*"+b+"=" + area(a,b));
 	}
 
+	public static void hello(String world){
+		System.out.println("Hello, " + world+"!");
+	}
+
+	public static double area(double dlina){
+		return dlina * dlina;
+	}
+
+	public static double area(double a, double b){
+		return a*b;
+	}
 }
